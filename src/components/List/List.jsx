7b41c7bbdd6954ae944b652ae47d0d605db16697
Shengxin Chen <124843788+SeanXC@users.noreply.github.com>
2025-09-18 22:19:00 +0100

    feat: implement List and PlaceDetails components

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+import useStyles from './styles';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
 
 const List = () => {
+    const classes = useStyles();
+    const [type, setType] = useState('restaurant');
+    const [rating, setRating] = useState('');
+    const places = [
+        { name: 'cool place'},
+        { name: 'best beer'},
+        { name: 'best coffee'},
+        { name: 'best pizza'},
+        { name: 'best steak'},
+        { name: 'best sushi'},
+        { name: 'best ramen'},
+        { name: 'best taco'},
+        { name: 'best burger'},
+        { name: 'best ice cream'},
+    ];
+
     return (
-        <h1>List</h1>
+        <div className={classes.container}>
+            <Typography variant="h4">Restaurants, Hotels & Attractions around you</Typography>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Type</InputLabel>
+                <Select value={type} onChange={(e) => setType(e.target.value)}>
+                    <MenuItem value="restaurant">Restaurant</MenuItem>
+                    <MenuItem value="hotel">Hotel</MenuItem>
+                    <MenuItem value="attraction">Attraction</MenuItem>
+                </Select>
+            </FormControl>
+            <FormControl className={classes.formControl}>
+                <InputLabel>Rating</InputLabel>
+                <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                    <MenuItem value="0">All</MenuItem>
+                    <MenuItem value="3">Above 3.0</MenuItem>
+                    <MenuItem value="4">Above 4.0</MenuItem>
+                    <MenuItem value="4.5">Above 4.5</MenuItem>
+                </Select>
+            </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place, i) => (
+                    <Grid item xs={12} key={i}>
+                        <PlaceDetails place={place} />
+                    </Grid>
+                ))}
+            </Grid>
+        </div>
     );
 };
 
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -9,7 +9,7 @@ const Map = () => {
     return (
         <div className={classes.mapContainer}>
             <GoogleMapReact
-                bootstrapURLKeys={{ key: '' }}
+                bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
                 defaultCenter={coordinates}
                 center={coordinates}
                 defaultZoom={14}
