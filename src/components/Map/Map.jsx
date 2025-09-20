70cdbd3528e804251993ccb73f6517bccecc3030
Shengxin Chen <124843788+SeanXC@users.noreply.github.com>
2025-09-20 01:50:05 +0100

    feat: Display Places on the Map

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOn';
 import useStyles from './styles';
+import Rating from '@material-ui/lab/Rating';
+import { Place } from '@material-ui/icons';
 
-const Map = ({ setCoordinates, setBounds, coordinates }) => {
+const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
     const classes = useStyles();
+    const isDesktop = useMediaQuery('(min-width:600px)');
 
     return (
         <div className={classes.mapContainer}>
@@ -20,6 +25,30 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
                 }}
                 onChildClick={''}
             >
+                {places?.map((place, i) => (
+                    <div
+                        className={classes.markerContainer}
+                        lat={Number(place.latitude)}
+                        lng={Number(place.longitude)}
+                        key={i}
+                    >
+                        {
+                            !isDesktop ? (
+                                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+                            ) : (
+                                <Paper elevation={3} className={classes.paper}>
+                                    <Typography className={classes.typography} variant="subtitle2">{place.name}</Typography>
+                                    <img 
+                                        className={classes.pointer}
+                                        src={place.photo ? place.photo.images.large.url : 'https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg'}
+                                        alt={place.name}
+                                    />
+                                    <Rating name="read-only" value={Number(place.rating)} readOnly />
+                                </Paper>
+                            )
+                        }
+                    </div>
+                ))}
             </GoogleMapReact>
         </div>
     );
